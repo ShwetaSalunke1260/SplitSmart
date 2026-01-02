52dbb481622a14f7f8ea684b919ca63382a50699
Shweta Salunke <shweta.dattatraya.salunke@gmail.com>
2026-01-02 19:47:43 +0530

    Add JWT login endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter # type:ignore
+from rest_framework.routers import DefaultRouter #type:ignore
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView #type:ignore
 
 from .views import (
     RegisterView,
@@ -8,17 +9,23 @@ from .views import (
     GroupViewSet,
     ExpenseViewSet,
     SettlementViewSet,
-    UserViewSet,
+    UserViewSet
 )
 
 router = DefaultRouter()
 router.register(r"groups", GroupViewSet, basename="group")
 router.register(r"expenses", ExpenseViewSet, basename="expense")
 router.register(r"settlements", SettlementViewSet, basename="settlement")
-router.register("users", UserViewSet, basename="users")
+router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
+    # 🔓 AUTH (PUBLIC)
     path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    # 🔐 AUTHENTICATED
     path("auth/me/", CurrentUserView.as_view(), name="current_user"),
+
     path("", include(router.urls)),
 ]
