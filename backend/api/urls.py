46a195ce762ac5c5b9b0d4d5150ded709ef77f16
Shweta Salunke <shweta.dattatraya.salunke@gmail.com>
2026-01-02 20:57:46 +0530

    Fix PDF group name and update reports page

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter #type:ignore
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView #type:ignore
+from rest_framework.routers import DefaultRouter # type:ignore
 
 from .views import (
     RegisterView,
@@ -9,23 +8,17 @@ from .views import (
     GroupViewSet,
     ExpenseViewSet,
     SettlementViewSet,
-    UserViewSet
+    UserViewSet,
 )
 
 router = DefaultRouter()
 router.register(r"groups", GroupViewSet, basename="group")
 router.register(r"expenses", ExpenseViewSet, basename="expense")
 router.register(r"settlements", SettlementViewSet, basename="settlement")
-router.register(r"users", UserViewSet, basename="users")
+router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
-    # 🔓 AUTH (PUBLIC)
     path("auth/register/", RegisterView.as_view(), name="register"),
-    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
-    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
-    # 🔐 AUTHENTICATED
     path("auth/me/", CurrentUserView.as_view(), name="current_user"),
-
     path("", include(router.urls)),
 ]
